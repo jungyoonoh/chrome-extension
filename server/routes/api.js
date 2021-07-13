8e48b83c78b9a7d2c464a8f895190b55928d5d83
Yoonoh <xkakak142@naver.com>
2021-07-13 17:53:44 +0900

    get youtube search data from API

---
+++ server/routes/api.js
@@ -0,0 +1,40 @@
+// YOUTUBE DATA API v3. Search
+// 파라미터 가이드 : https://developers.google.com/youtube/v3/docs/search
+
+const express = require('express');
+const router = express.Router();
+require('dotenv').config({path: "../credentials/.env"});
+
+const request = require('request');
+
+// 영문 검색시
+var keyword = "Gfriend Eunha";
+
+var optionParams = {
+    q:keyword,
+    part:"snippet",
+    key:process.env.GCP_API_KEY,
+    maxResults:5
+};
+
+// 한글 검색어 사용시 인코딩 과정 필요
+// optionParams.q = encodeURI(optionParams.q);
+
+var url = "https://www.googleapis.com/youtube/v3/search?";
+
+for(var option in optionParams){
+    url += option + "=" + optionParams[option]+"&";
+}
+
+url = url.substr(0, url.length - 1);
+
+// TEST
+// https://www.youtube.com/watch?v=QYJDDAml6M0
+// v = obj[id][videoId] value
+request.get(url, (err, res, body) => {
+    console.log(body);
+});
+
+router.get('/youtube', (req, res) => {
+    // for display data
+})
