a0a2539ada0390cc41a65d48f0bb366cea445b4f
Yoonoh <xkakak142@naver.com>
2021-07-19 16:19:27 +0900

    set proxy and dual build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import 'css/App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios"
 
 function App() {
+
+  const [testValue, setTestValue] = useState(null); // 변수
+
+  const testApi = async () => {
+    await axios.get('/api/news')
+      .then(response => {
+        console.log(response);
+      }).catch(err => {console.log(err)});
+  }
+
+  useEffect(() => {
+    testApi();
+  }, [testValue]);
+
   return (
     <div className="App">
-      고건준과 함께 춤을
+      고건준과 함께 춤을<br></br>
+      <button id="test" onClick={testApi}>
+        버튼을 누르세요
+      </button>
     </div>
   );
 }
