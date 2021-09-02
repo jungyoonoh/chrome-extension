9ba37c0007aa12948951ee44e3010c2be4b085c3
bb2sol <iceteateemo@gmail.com>
2021-09-02 14:35:49 +0900

    MainSlide: card 컴포넌트 추가, 디자인 변경

--- server/server.js
+++ server/server.js
@@ -10,16 +10,16 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
-const {sequelize}=require('./database/models');
+// const {sequelize}=require('./database/models');
 
 app.use(express.json()); // Express v4.16.0 기준 built-in body-parser 포함
 app.use(express.urlencoded({extend:true}));
 app.use(cors());
 app.use(cookieParser());
 
-sequelize.sync({force:true}).then(()=>{//실행할때마다 생성쿼리 실행(업데이트 시 사용)
-    console.log('연결 성공');
-})
+// sequelize.sync({force:true}).then(()=>{//실행할때마다 생성쿼리 실행(업데이트 시 사용)
+//     console.log('연결 성공');
+// })
 
 app.use('/api', api);
 
--- src/slides/MainSlide.js
+++ src/slides/MainSlide.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
 import 'css/MainSlide.css';
 import axios from "axios"
-import {GrFormRefresh, } from 'react-icons/gr'
+import {GrFormRefresh, GrYoutube, } from 'react-icons/gr'
+import {BiSearch, BiNews} from 'react-icons/bi'
+import {RiStockFill, } from 'react-icons/ri'
+import CardM from "../components/CardM"
 
 function MainSlide() {
     // 1. 구글 검색창
@@ -47,7 +50,23 @@ function MainSlide() {
     // thumbnails: "https://i.ytimg.com/vi/LABr_O5MQKY/hqdefault.jpg"
     // title: "ㅁㄴㅇㄹ"
     // videoUrl: "https://www.youtube.com/watch?v=LABr_O5MQKY"
-
+    const [youtubeTop5, setYoutubeTop5] = useState([])
+    const [isYoutubeLoaded, setIsYoutubeLoaded] = useState(false)
+    useEffect(()=>{
+        if (!isYoutubeLoaded){
+            axios.get(
+                '/api/youtube'
+              ).then(response => {
+                console.log(response.data);
+                let data = response.data
+                setYoutubeTop5(data)
+              }).catch(err => {console.log(err)});
+              setIsYoutubeLoaded(true)
+        }
+    },[isYoutubeLoaded])
+    const openYoutubeDetail =(url)=>{
+        window.open(url)
+    }
     // 5. 뉴스 top 5
     const [newsTop5, setNewsTop5] = useState([])
     const [isNewsLoaded, setIsNewsLoaded] = useState(false)
@@ -71,24 +90,26 @@ function MainSlide() {
         <div style={{}}>
             <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
             <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-            <div style={{display:'flex', flexDirection:'row'}}>
+            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-around'}}>
                 <div>
-                    <div className='google-search' style={{backgroundColor:'pink'}}>
-                        <input value={searchTerm} onChange={onChange} onFocus={()=>{setIsInputFocused(true)}} onBlur={()=>{setIsInputFocused(false)}} onKeyPress={onCheckEnter}/>
+                    <div className='google-search' style={{}}>
+                        <div className='google-search-left'></div>
+                        <input className='google-search-input' value={searchTerm} onChange={onChange} onFocus={()=>{setIsInputFocused(true)}} onBlur={()=>{setIsInputFocused(false)}} onKeyPress={onCheckEnter}/>
                         <button 
                             className='google-search-button'
                             onClick={googleSearch}>
-                            검색
+                            <BiSearch style={{width:30, height:30, color:'white'}}/>
                         </button>
                     </div>
                     <div className='frequent-sites' style={{backgroundColor:'yellow'}}>
                         
                     </div>
                 </div>
-                <div className='stock-top5' style={{backgroundColor:'blue'}}>
-                    <div style={{display:'flex', flexDirection:'row'}}>
-                        <p>주식 거래량 Top 5</p>
-                        <button className='refresh-button' onClick={()=>{setIsStockLoaded(false)}}><GrFormRefresh /></button>
+                <div className='stock-top5'>
+                    <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                        <RiStockFill style={{width:40, height:40, marginRight:5}}/>
+                        <h1>주식 거래량 Top 5</h1>
+                        <button className='refresh-button' onClick={()=>{setIsStockLoaded(false)}}><GrFormRefresh style={{width:40, height:40}}/></button>
                     </div>
                     {stockTop5.map((item)=>{
                         var dir = item.dir
@@ -106,18 +127,33 @@ function MainSlide() {
                     })}
                 </div>
             </div>
-            <div style={{display:'flex', flexDirection:'row'}}>
-                <div className='youtube-top5' style={{backgroundColor:'green'}}>
-
+            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-around'}}>
+                <div className='youtube-top5'>
+                    <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                        <GrYoutube style={{width:40, height:40, marginRight:5}}/>
+                        <h1>유튜브 인기 동영상</h1>
+                    </div>
+                    {youtubeTop5.map((item, index)=>{
+                        return(
+                            <div
+                                className='youtube'
+                                onClick={()=>{openYoutubeDetail(item.thumbnails)}}>
+                                <CardM title={item.title} thumbnail={item.thumbnails} name={item.channelTitle}/>
+                            </div>
+                        )
+                    })}
                 </div>
-                <div className='news-top5' style={{backgroundColor:'aqua'}}>
+                <div className='news-top5'>
+                    <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                        <BiNews style={{width:40, height:40, marginRight:5}}/>
+                        <h1>언론사별 가장 많이 본 뉴스</h1>
+                    </div>
                     {newsTop5.map((item, index)=>{
                         return(
                             <div
                                 className='news'
                                 onClick={()=>{openNewsDetail(item.thumb)}}>
-                                <p>{item.comp}</p>
-                                <p>{item.title}</p>
+                                <CardM title={item.title} thumbnail={item.thumb} name={item.comp}/>
                             </div>
                         )
                     })}
