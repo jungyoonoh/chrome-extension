08d6323705290815d58d0e463552ad7ab9a5a992
Yoonoh <xkakak142@naver.com>
2022-02-27 12:58:54 +0900

    feat : add moment, weather section

--- src/slides/StockSlide.js
+++ src/slides/StockSlide.js
@@ -40,58 +40,58 @@ const StockSlide=()=>{
           }).catch(err => {console.log(err)});
     }, []);
 
-    useEffect(() => {
-        axios
-            .post(
-                '/api/stock', 
-                {keyword:stockList})
-            .then(response => {
-                console.log("response", response);
-                setStockList(response.data);
-            })
-            .catch(err => {console.log(err)});
-    }, [keywords]);
+    // useEffect(() => {
+    //     axios
+    //         .post(
+    //             '/api/stock', 
+    //             {keyword:stockList})
+    //         .then(response => {
+    //             console.log("response", response);
+    //             setStockList(response.data);
+    //         })
+    //         .catch(err => {console.log(err)});
+    // }, [keywords]);
     
-    return (
-        <>
-            <div className="keyword-container">
-                {keywords.map((item)=>{
-                    return (
-                        <div className="keyword" onClick={()=>{deleteKeyword(item)}}>{item}</div>
-                    )
-                })}
-                {keywords.length < maxLength &&
-                    <div className="add-keyword-container">
-                        <input value={keyword} onChange={onChange} className="add-keyword-input" maxLength={10}/> 
-                        <button onClick={addKeyword} className="add-keyword-button">+</button>
-                    </div>
-                }
-            </div>
-            <div className='stock_list'>
-                <div className='list_box'>
-                    {stockList.map((item, index)=> { 
-                        if(index < 5){
-                            return (
-                                <div className="list_item">
-                                    {/* <CardM title={item.title} thumbnail={item.thumbnails} name={item.channelTitle} url={item.videoUrl}/> */}
-                                </div>
-                            )
-                        }
-                    })}
-                </div>
-                <div className='list_box'>
-                    {stockList.map((item, index)=> { 
-                        if(index >= 6 && index < 11){
-                            return(
-                                <div className="list_item">
-                                    {/* <CardM title={item.title} thumbnail={item.thumbnails} name={item.channelTitle} url={item.videoUrl}/> */}
-                                </div>
-                            )
-                        }
-                    })}
-                </div>
-            </div>
-        </>
+    return (<div></div>
+        // <>
+        //     <div className="keyword-container">
+        //         {keywords.map((item)=>{
+        //             return (
+        //                 <div className="keyword" onClick={()=>{deleteKeyword(item)}}>{item}</div>
+        //             )
+        //         })}
+        //         {keywords.length < maxLength &&
+        //             <div className="add-keyword-container">
+        //                 <input value={keyword} onChange={onChange} className="add-keyword-input" maxLength={10}/> 
+        //                 <button onClick={addKeyword} className="add-keyword-button">+</button>
+        //             </div>
+        //         }
+        //     </div>
+        //     <div className='stock_list'>
+        //         <div className='list_box'>
+        //             {stockList.map((item, index)=> { 
+        //                 if(index < 5){
+        //                     return (
+        //                         <div className="list_item">
+        //                             {/* <CardM title={item.title} thumbnail={item.thumbnails} name={item.channelTitle} url={item.videoUrl}/> */}
+        //                         </div>
+        //                     )
+        //                 }
+        //             })}
+        //         </div>
+        //         <div className='list_box'>
+        //             {stockList.map((item, index)=> { 
+        //                 if(index >= 6 && index < 11){
+        //                     return(
+        //                         <div className="list_item">
+        //                             {/* <CardM title={item.title} thumbnail={item.thumbnails} name={item.channelTitle} url={item.videoUrl}/> */}
+        //                         </div>
+        //                     )
+        //                 }
+        //             })}
+        //         </div>
+        //     </div>
+        // </>
     )
 }
 
